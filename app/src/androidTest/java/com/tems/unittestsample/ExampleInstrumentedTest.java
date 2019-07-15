fd0dc6f8d78173b558b09aa3e46c4f99eb7565d2
See Chee Ng <see-chee.ng@infovista.com>
2019-07-15 19:31:20 +0800

    Added new ui test ensureNameIsExist

--- app/src/androidTest/java/com/tems/unittestsample/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/tems/unittestsample/ExampleInstrumentedTest.java
@@ -1,9 +1,15 @@
 package com.tems.unittestsample;
 
+import android.app.Instrumentation;
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
 
+import androidx.test.uiautomator.By;
+import androidx.test.uiautomator.BySelector;
+import androidx.test.uiautomator.UiDevice;
+
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -16,11 +22,24 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+
     @Test
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
         assertEquals("com.tems.unittestsample", appContext.getPackageName());
+    }
+
+    @Test
+    public void ensureNameIsExist() {
+        Instrumentation instrumentation = InstrumentationRegistry.getInstrumentation();
+
+        UiDevice device = UiDevice.getInstance(instrumentation);
+        Context context = instrumentation.getContext();
+
+        BySelector nameField = By.clazz("android.widget.EditText").res("com.tems.unittestsample:id/userNameInput");
+
+        assertEquals(nameField != null, nameField != null);
     }
 }
